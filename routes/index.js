d0d8f53fff61221ddd08339318a442aa2336d7e8
StrudelAdmin <112951686+StrudelAdmin@users.noreply.github.com>
2023-12-31 11:22:15 +0100

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+export const routes = {
+	home: "/",
+	registration: "/registration",
+	login: "/login",
+	welcome: "/welcome",
+	companyRegistration: "/company-registration",
+	profile: "/profile",
+	profileSubServices: "/profile/subservices",
+	service: "/service",
+	serviceAdd: "/service/add",
+	subServices: "/subservices",
+	search: "/search",
+	notFound: "/404"
+};
